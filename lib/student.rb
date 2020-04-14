9f782300dd0c6c7d5410f9694d0045b02437106b
Jamal Rjoul <jamensky1991@gmail.com>
2020-04-14 21:13:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,7 +26,7 @@ class Student
   end
 
   def save
-    sql = INSERT INTO students(name, grade) VALUES(?,?)
+    sql = 'INSERT INTO students(name, grade) VALUES(?,?)'
     DB[:conn].execute(sql, self.name, self.grade)
   end
 
