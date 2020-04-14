8e1595f5b8a3cec2913264f47502bd2e9b8f7de4
Jamal Rjoul <jamensky1991@gmail.com>
2020-04-14 20:46:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,14 +1,14 @@
 class Student
-  attr_reader :id 
+  attr_reader :id
   attr_accessor :name, :grade
-  
+
   def initialize(name, grade)
-    @name = name 
-    @grade = grade 
-  end 
+    @name = name
+    @grade = grade
+  end
 
   def self.create_table
-    
-  end 
-  
+
+  end
+
 end
