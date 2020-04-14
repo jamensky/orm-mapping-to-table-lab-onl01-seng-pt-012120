c5a29673c2e244980894b5ad55eaf840ab644f41
Jamal Rjoul <jamensky1991@gmail.com>
2020-04-14 21:14:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ class Student
   attr_reader :id
   attr_accessor :name, :grade
 
-  def initialize(name, grade, id=nill)
+  def initialize(name, grade, id=nil)
     @name = name
     @grade = grade
     @id = id
