8817b64c45e0e5e000107b67c59cfa500e0f5555
Jamal Rjoul <jamensky1991@gmail.com>
2020-04-14 21:18:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,6 +28,7 @@ class Student
   def save
     sql = 'INSERT INTO students(name, grade) VALUES(?,?)'
     DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute('SELECT last_row_id() FROM students')[0][0]
   end
 
 end
